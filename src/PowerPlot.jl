58922939b5987b5fa8d235a5d34b6277b5b78089
mwsohn@gmail.com <mwsohn@gmail.com>
2017-09-02 11:45:35 -0400

    plot changed

--- src/PowerPlot.jl
+++ src/PowerPlot.jl
@@ -1,9 +1,9 @@
 using pwr, Plots
 
 #f2 = pwr.F2Test(u=12,v=99,f2=.3,power=0.0)
-import RecipesBase.plot
+#import RecipesBase.plot
 
-function plot(ht::pwr.htest)
+function pwrplot(ht::pwr.htest)
 
     methods = (
         "One-sample t-test power calculation",
