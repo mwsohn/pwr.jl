9a56f70a7213224fc03c9767029d4d8e9d2047fd
mwsohn@gmail.com <mwsohn@gmail.com>
2017-09-04 01:03:34 -0400

    PowerPlot updated

--- src/PowerPlot.jl
+++ src/PowerPlot.jl
@@ -199,7 +199,7 @@ function plot(ht::pwr.htest)
     plot(df[:x],df[:y],title = title_string, xlabel = xlab_string,ylabel = ylab_string,
         yticks=[0.0 0.2 0.4 0.6 0.8 1.0],xlim=(10,n_upper),
         label=false,legend = false,
-        annotations=(3,3,text(legend_string)))
+        annotations=(20,20,text(legend_string),16,:black,:left))
 
     # add options
 
