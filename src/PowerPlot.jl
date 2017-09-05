f179ee0214adccf5183c45dd6ffb6119c72956ee
Min-Woong Sohn <Min-Woong Sohn>
2017-09-05 14:39:04 -0400

    plot fine-tuning

--- src/PowerPlot.jl
+++ src/PowerPlot.jl
@@ -230,7 +230,7 @@ function plot(ht::pwr.htest)
     # select the backend
     plotly()
 
-    xlim_lower = sample_sizes[20] / 800
+    xlim_lower = sample_sizes[20] / 500
 
     # plot with title and x-axis and y-axis labels
     plot(df[:x],df[:y],
