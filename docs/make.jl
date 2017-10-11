bce85cbab94f17cd4ac763a677b5a1c81133c952
Min-Woong Sohn <mwsohn@gmail.com>
2017-10-11 03:13:53 -0400

    travis.yml updated

--- docs/make.jl
+++ docs/make.jl
@@ -5,5 +5,8 @@ push!(LOAD_PATH,"../src/")
 makedocs()
 
 deplydocs(
+    deps   = Deps.pip("mkdocs", "python-markdown-math"),
     repo = "github.com/mwsohn/pwr.jl.git"
+    julia  = "0.6",
+    osname = "osx"
 )
