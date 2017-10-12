944396f1f9172ef6ab49a43606753d8e05cd4f72
Min-Woong Sohn <mwsohn@gmail.com>
2017-10-11 21:49:45 -0400

    make.jl modified

--- docs/make.jl
+++ docs/make.jl
@@ -4,9 +4,9 @@ push!(LOAD_PATH,"../src/")
 
 makedocs()
 
-deplydocs(
-    deps   = Deps.pip("mkdocs", "python-markdown-math"),
-    repo = "github.com/mwsohn/pwr.jl.git"
-    julia  = "0.6",
+deploydocs(
+    deps = Deps.pip("mkdocs", "python-markdown-math"),
+    repo = "github.com/mwsohn/pwr.jl.git",
+    julia = "0.6",
     osname = "osx"
 )
